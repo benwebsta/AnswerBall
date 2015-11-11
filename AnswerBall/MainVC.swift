86773dd50039104235baed2b664e5beca045ecbd
benwebsta <bmwebster@wisc.edu>
2015-11-11 15:01:46 -0600

    finalized copy

--- AnswerBall/MainVC.swift
+++ AnswerBall/MainVC.swift
@@ -16,23 +16,53 @@ class MainVC: UIViewController, AVAudioPlayerDelegate {
     @IBOutlet weak var label: UILabel!
     
     var answers = [
-        "Suck My Dick, OR COCK!",
-        "Fuck you kid, youre a dick!",
-        "BOB SAGGIT!",
-        "FUCK SALT",
-        "DAMNIT these fish stick are as hard as tits!",
-        "BITCH, i love you!",
-        "SHIT.... SHIT........ SHIIIIIIT!",
-        "ID KILL MYSELF TOO IF MY LAST NAME WAS COOOOOMBS",
-        "PISSING OUT THE WINDOW, AND SHITTING OUT THE WINDOW ARE TWO DIFFERENT THINGS!!",
-        "SUCK MY DICK",
+        "I'm not a mindreader!",
+        "All indicators point to YES!",
+        "All indicators point to NO!",
+        "Watch your language!",
+        "It seems highly unlikely.",
+        "Please speak up!",
+        "It seems highly likely.",
+        "Yes, indeed!",
+        "Absolutely not!",
+        "Please ask again later...",
+        "Seriously?",
+        "That was a dumb question.",
+        "I was wondering the same thing!",
+        "I can't answer that truthfully.",
+        "What do you think?",
+        "OMG YES!",
+        "OMG NO!",
+        "Imagine that!",
+        "Really?",
+        "Did you just ask that?",
+        "Of course.",
+        "Ask a higher power.",
+        "The force is with you.",
+        "The force is not near.",
+        "You're unbelievable!",
+        "Can I get a translation please?",
+        "I'm gonna guess... NO!",
+        "I'm gonna guess... YES!",
+        "It would seem so.",
+        "That does not seem to be true.",
+        "Does a bear sh... never mind.",
+        "I need a new profession!",
+        "The odds are against that.",
+        "The odds are in your favor.",
+        "Can we skip this one?",
+        "Ask a different question please.",
+        "I'm doing good, yes I know you didn't ask.",
+        "My connection to the stars was lost.",
+        "Do you want the truth?",
+        "I can see no answer but yes."
     ]
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         
-        label.text = "Rudy is a pussy"
+        label.text = "Ask me a question, human."
     }
 
     override func didReceiveMemoryWarning() {
